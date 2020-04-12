ef203fa59723efa6a6c6c06ff9df971adc71e14e
Maryam <bellomaryam351@gmail.com>
2020-04-12 16:15:46 +0100

    my project

---
+++ index.js
@@ -0,0 +1,19 @@
+function myFunction1(){
+  var x = document.getElementById("dropdown1");
+
+}
+function myFunction2(){
+    document.getElementById("dropdown2").classList.toggle("show");
+}
+function myFunction1(){
+    document.getElementById("dropdown3").classList.toggle("show");
+}
+function myFunction1(){
+    document.getElementById("dropdown4").classList.toggle("show");
+}
+function myFunction1(){
+    document.getElementById("dropdown5").classList.toggle("show");
+}
+function myFunction1(){
+    document.getElementById("dropdown6").classList.toggle("show");
+}
